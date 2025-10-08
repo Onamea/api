cb2f1587125d992c35c0f154086b34d012e48a85
Mike O'Bank <mike.obank@pm.me>
2025-10-08 15:27:04 +0200

    Script signData

---
+++ scripts/signData.ts
@@ -0,0 +1,39 @@
+import { isName, isPrimaryKey, primaryKeyToPublicKey, sign, fromHex, keyPairFromPrivateKey, displayPublicKey } from "@vanice/types"
+
+const name = Deno.args[0]
+const primaryKey = Deno.args[1]
+const privateKeyHex = Deno.args[2]
+
+if (isName(name) === false) {
+  console.error("Invalid name")
+  Deno.exit(1)
+}
+
+if (isPrimaryKey(primaryKey) === false) {
+  console.error("Invalid primary key")
+  Deno.exit(1)
+}
+
+if (privateKeyHex === undefined) {
+  console.error("Missing private key")
+  Deno.exit(1)
+}
+
+const publicKey = primaryKeyToPublicKey(primaryKey)
+const privateKey = fromHex(privateKeyHex)
+const keyPair = keyPairFromPrivateKey(privateKey)
+
+if (displayPublicKey(publicKey) !== displayPublicKey(keyPair[0])) {
+  console.error("Private key does not match primary key")
+  Deno.exit(1)
+}
+
+const signature = await sign(primaryKey, name, privateKey)
+
+const data = { primaryKey, name, signature }
+
+console.log(`
+  curl -X POST \
+  -H "Content-Type: application/json" \
+  -d '${ JSON.stringify(data) }' \
+  https://api.vanice.cloud/`)
