1c331ff59dea00b69e36bba4a54859eb379b1954
Mike O'Bank <mike.obank@pm.me>
2026-03-12 19:08:44 +0530

    Fix clear migration

--- migrations/clear.ts
+++ migrations/clear.ts
@@ -1,8 +1,12 @@
 const kv = await Deno.openKv()
 
 export default async () => {
-  const entries = kv.list({ prefix: ["vanice"] })
-  for await (const entry of entries) {
+  const identities = kv.list({ prefix: ["identities"] })
+  for await (const entry of identities) {
+    kv.delete(entry.key)
+  }
+  const items = kv.list({ prefix: ["items"] })
+  for await (const entry of items) {
     kv.delete(entry.key)
   }
 }
