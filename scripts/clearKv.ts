d4cefb6b8a186e947630601b21784b0279e16e6a
Mike O'Bank <mike.obank@pm.me>
2026-08-10 18:59:20 +0200

    Clean up scripts

--- scripts/clearKv.ts
+++
@@ -1,7 +0,0 @@
-const kv = await Deno.openKv()
-const entries = kv.list({ prefix: [] })
-for await (const entry of entries) {
-  await kv.delete(entry.key)
-}
-console.log("KV database cleared")
-kv.close()
